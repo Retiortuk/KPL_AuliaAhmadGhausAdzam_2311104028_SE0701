4bff01a0c3dbd782decf159dcd084f915cdf2a16
RetiortUK <aauliagazzam@gmail.com>
2025-02-24 23:23:50 +0700

    feat: a things that user want to buy

--- 02_Pengenalan_IDE_dan_Pemrograman_NodeJS/challenge_praktikum2.js
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJS/challenge_praktikum2.js
@@ -1,2 +1,11 @@
 // Aulia Ahmad Ghaus Adzam
 // 2311104028
+
+// Barang Yang DiBeli Misalnya
+const items = [
+    { name: 'Barang Ke 1', price: 50000},
+    { name: 'Barang ke 2', price: 10000},
+    { name: 'Barang ke 3', price: 12000}
+];
+
+let totalPrice = items.reduce((sum, item) => sum + item.price, 0);
