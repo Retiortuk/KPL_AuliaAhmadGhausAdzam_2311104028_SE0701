a210b30316eb68d28d4260b334069f44a18110c1
RetiortUK <aauliagazzam@gmail.com>
2025-03-10 11:23:31 +0700

    feat: oop

---
+++ 04_oop/management.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const person = new Person('John', 30);
+console.log(person.name);
+console.log(person.age);
