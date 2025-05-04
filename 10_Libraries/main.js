bad07a63ed2460bf0fe3adddf55e22f9fe32a6fb
RetiortUK <aauliagazzam@gmail.com>
2025-05-04 20:43:11 +0700

    feat: jurnal

---
+++ 10_Libraries/main.js
@@ -0,0 +1,13 @@
+
+
+const { FPB, KPK, Turunan, Integral } = require('./matematikaLibraries');
+
+console.log("=== PEMANGGILAN LIBRARY MATEMATIKA ===");
+console.log("FPB(60, 45):", FPB(60, 45)); 
+console.log("KPK(12, 8):", KPK(12, 8));   
+
+console.log("\nTurunan([1, 4, -12, 9]):", Turunan([1, 4, -12, 9]));
+
+
+console.log("\nIntegral([4, 6, -12, 9]):", Integral([4, 6, -12, 9]));
+
