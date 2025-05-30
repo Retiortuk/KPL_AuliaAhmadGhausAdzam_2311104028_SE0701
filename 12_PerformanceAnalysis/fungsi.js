be3244c142983fec3877a47afc6f881f81d1d763
Aulia Ahmad <141942230+Retiortuk@users.noreply.github.com>
2025-05-30 14:26:17 +0700

    Delete 12_PerformanceAnalysis directory

--- 12_PerformanceAnalysis/fungsi.js
+++
@@ -1,7 +0,0 @@
-function CariTandaBilangan(a) {
-    if (a < 0) return 'Negatif';
-    if (a > 0) return 'Positif';
-    return 'Nol';
-}
-
-module.exports = { CariTandaBilangan };
--- 12_PerformanceAnalysis/renderer.js
+++
@@ -1,7 +0,0 @@
-const { CariTandaBilangan } = require('./fungsi');
-
-window.cekTanda = function () {
-    const input = parseInt(document.getElementById('inputNumber').value);
-    const hasil = CariTandaBilangan(input);
-    document.getElementById('outputLabel').textContent = hasil;
-}
